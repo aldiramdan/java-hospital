51b9322fab99a97851edf9e319a7d97573b8aaf7
aldiramdan <aldi.ramdan96@gmail.com>
2023-08-28 15:57:30 +0700

    Optimize import

--- src/main/java/com/aldiramdan/hospital/model/dto/request/PatientRequest.java
+++ src/main/java/com/aldiramdan/hospital/model/dto/request/PatientRequest.java
@@ -1,6 +1,9 @@
 package com.aldiramdan.hospital.model.dto.request;
 
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
