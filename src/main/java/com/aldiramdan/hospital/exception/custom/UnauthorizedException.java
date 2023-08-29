25936f5ee0ed0a6e01d90e919320c14e023f4eba
aldiramdan <aldi.ramdan96@gmail.com>
2023-08-29 09:27:12 +0700

    Revert "Remove custom exeception unauthorized"
    This reverts commit 4452008b498fcaa54f832631266a94c1c1b809e0.

---
+++ src/main/java/com/aldiramdan/hospital/exception/custom/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.aldiramdan.hospital.exception.custom;
+
+public class UnauthorizedException extends Exception {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
